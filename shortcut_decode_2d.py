f0053dee26f90549baff78260a2f984ec34de21b
Emily Irvine <emilymirvine@gmail.com>
2016-09-07 09:48:11 -0400

    Evaluating 2D decoding
    Parameter check for smoothing kernel over position and time.

--- shortcut_decode_2d.py
+++ shortcut_decode_2d.py
@@ -26,12 +26,16 @@ thisdir = os.path.dirname(os.path.realpath(__file__))
 pickle_filepath = os.path.join(thisdir, 'cache', 'pickled')
 output_filepath = os.path.join(thisdir, 'plots', 'decode')
 
+results = []
 
-infos = [r063d3]
+# infos = [r063d3]
 # infos = [r063d2, r063d3, r063d4, r063d5, r063d6, r066d1, r066d2, r066d3, r066d4, r067d1]
 
-for info in infos:
-    print(info.session_id)
+def check_tc_smoothing(smooth_tc, smooth_counts):
+# for info in infos:
+#     print(info.session_id)
+    info = r063d2
+    print(smooth_tc, smooth_counts)
     position = get_pos(info.pos_mat, info.pxl_to_cm)
     spikes = get_spikes(info.spike_mat)
 
@@ -50,11 +54,11 @@ for info in infos:
     xedges = np.arange(track_pos.x.min(), track_pos.x.max() + binsize, binsize)
     yedges = np.arange(track_pos.y.min(), track_pos.y.max() + binsize, binsize)
 
-    tuning_curves = vdm.tuning_curve_2d(track_pos, track_spikes, xedges, yedges, gaussian_sigma=0.2)
+    tuning_curves = vdm.tuning_curve_2d(track_pos, track_spikes, xedges, yedges, gaussian_sigma=smooth_tc)
 
     counts_binsize = 0.025
     time_edges = get_edges(run_pos, counts_binsize, lastbin=True)
-    counts = vdm.get_counts(spikes, time_edges, gaussian_std=counts_binsize)
+    counts = vdm.get_counts(spikes, time_edges, gaussian_std=smooth_counts)
 
     decoding_tc = []
     for tuning_curve in tuning_curves:
@@ -85,12 +89,25 @@ for info in infos:
 
     errors = actual_position.distance(decoded)
     print('Actual distance:', np.mean(errors))
+    return np.mean(errors)
 
     # plt.plot(actual_position.x, actual_position.y, 'r.', ms=0.7)
     # plt.plot(decoded.x, decoded.y, 'b.')
     # plt.show()
 
-    test_xy = actual_position.data + np.random.normal(0, 24, actual_position.data.shape)
-    test_pos = vdm.Position(test_xy, actual_position.time)
-    test_errors = actual_position.distance(test_pos)
-    print('Test distance:', np.mean(test_errors))
+    # test_xy = actual_position.data + np.random.normal(0, 24, actual_position.data.shape)
+    # test_pos = vdm.Position(test_xy, actual_position.time)
+    # test_errors = actual_position.distance(test_pos)
+    # print('Test distance:', np.mean(test_errors))
+
+inputs = []
+results = []
+
+smoothing_tc = [None, 0.1, 0.25, 0.5, 1., 3., 5., 7.5, 10., 15.]
+smoothing_time = [None, 0.025, 0.05, 0.1, 0.2, 0.5, 1., 2., 5.]
+for smooth_tc in smoothing_tc:
+    for smooth_t in smoothing_time:
+        inputs.append([smooth_tc, smooth_t])
+        results.append(check_tc_smoothing(smooth_tc, smooth_t))
+print('inputs:', inputs)
+print('results:', results)
