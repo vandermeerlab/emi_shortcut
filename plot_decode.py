Fixed plotting with track length normalization
And cleaned up some unused plotting functions.

plot_decode.py
@@ -4,7 +4,7 @@ import pickle
 from collections import OrderedDict
 from shapely.geometry import LineString
 
-from utils_plotting import plot_decoded_errors, plot_decoded_compare, plot_decoded_session_errors
+from utils_plotting import plot_decoded_compare, plot_decoded_session_errors
 
 thisdir = os.path.dirname(os.path.realpath(__file__))
 
@@ -12,32 +12,7 @@ pickle_filepath = os.path.join(thisdir, 'cache', 'pickled')
 output_filepath = os.path.join(thisdir, 'plots', 'decode')
 
 
-def get_zone_proportion(decoded, experiment_time):
-    """Computes the proportion of n_samples in each zone
-
-    Parameters
-    ----------
-    decoded: nept.Position
-
-    Returns: dict
-
-    """
-    if experiment_time not in ['prerecord', 'phase1', 'pauseA', 'phase2', 'pauseB', 'phase3', 'postrecord']:
-        raise ValueError("experiment time is not recognized as a shortcut experiment time.")
-
-    n_total = 0
-    for zone in ['u', 'shortcut', 'novel']:
-        n_total += decoded['zones'][zone].n_samples
-    n_total = np.maximum(n_total, 1.0)
-
-    decoded_proportions = dict()
-    for zone in ['u', 'shortcut', 'novel']:
-        decoded_proportions[zone] = decoded['zones'][zone].n_samples / n_total
-
-    return decoded_proportions
-
-
-def load_decoded(info, experiment_times, pickle_filepath, f_combine, shuffled=False, min_samples=15):
+def load_decoded(info, experiment_times, pickle_filepath, f_combine, shuffled=False):
     """Combines decoded outputs
 
     Parameters
@@ -55,7 +30,6 @@ def load_decoded(info, experiment_times, pickle_filepath, f_combine, shuffled=Fa
         with u, shortcut, novel, other as keys.
 
     """
-
     decode_together = OrderedDict()
 
     for experiment_time in experiment_times:
@@ -72,21 +46,89 @@ def load_decoded(info, experiment_times, pickle_filepath, f_combine, shuffled=Fa
         else:
             raise ValueError("pickled decoded not found for " + info.session_id)
 
-        decode_together[experiment_time] = f_combine(decoded, experiment_time)
+        decode_together[experiment_time] = f_combine(info, decoded, experiment_time)
 
     return decode_together
 
 
-def get_errors(decoded, experiment_time):
-    """Computes the error of decoded position compared to actual position
+def get_zone_proportion(info, decoded, experiment_time):
+    """Computes the proportion of n_samples in each zone
 
     Parameters
     ----------
+    info: module
     decoded: nept.Position
 
     Returns: dict
 
     """
+    # print('Working on', info.session_id)
+
+    if experiment_time not in ['prerecord', 'phase1', 'pauseA', 'phase2', 'pauseB', 'phase3', 'postrecord']:
+        raise ValueError("experiment time is not recognized as a shortcut experiment time.")
+
+    n_total = 0
+    for zone in ['u', 'shortcut', 'novel']:
+        n_total += decoded['zones'][zone].n_samples
+    n_total = np.maximum(n_total, 1.0)
+
+    decoded_proportions = dict()
+    for zone in ['u', 'shortcut', 'novel']:
+        decoded_proportions[zone] = decoded['zones'][zone].n_samples / n_total
+
+    return decoded_proportions
+
+
+def get_proportion_normalized(info, decoded, experiment_time):
+    """Finds decoded proportions normalized by track length
+
+    Parameters
+    ----------
+    info: module
+    decoded: nept.Position
+    experiment_time: str
+
+    Returns
+    -------
+    normalized_proportions: dict
+        With u, shortcut, novel as keys
+
+    """
+    # print('Working on', info.session_id)
+
+    if experiment_time not in ['prerecord', 'phase1', 'pauseA', 'phase2', 'pauseB', 'phase3', 'postrecord']:
+        raise ValueError("experiment time is not recognized as a shortcut experiment time.")
+
+    length = dict()
+    length['u'] = LineString(info.u_trajectory).length
+    length['shortcut'] = LineString(info.shortcut_trajectory).length
+    length['novel'] = LineString(info.novel_trajectory).length
+
+    n_total = 0
+    for zone in ['u', 'shortcut', 'novel']:
+        n_total += decoded['zones'][zone].n_samples / length[zone]
+    n_total = np.maximum(n_total, 1.0)
+
+    proportions_normalized = dict()
+    for zone in ['u', 'shortcut', 'novel']:
+        proportions_normalized[zone] = (decoded['zones'][zone].n_samples / length[zone]) / n_total
+
+    return proportions_normalized
+
+
+def get_errors(info, decoded, experiment_time):
+    """Computes the error of decoded position compared to actual position
+
+    Parameters
+    ----------
+    info: module
+    decoded: nept.Position
+
+    Returns: dict
+
+    """
+    # print('Working on', info.session_id)
+
     decoded_error = dict()
     for key in decoded['actual'].keys():
         if experiment_time in ['phase1', 'phase2', 'phase3']:
@@ -95,34 +137,6 @@ def get_errors(decoded, experiment_time):
             decoded_error[key] = 0
 
     return decoded_error
-
-
-def combine_errors(errors):
-    """Combines errors from  multiple sessions
-
-    Parameters
-    ----------
-    errors: list of OrderedDicts
-        With experiment times as keys, each a dict
-        with u, shortcut, novel as keys.
-
-    Returns
-    -------
-    combine_errors: OrderedDict
-        With experiment times as keys, each a dict
-        with u, shortcut, novel, together as keys.
-
-    """
-    combine_errors = OrderedDict()
-
-    for key in errors[0].keys():
-        combine_errors[key] = dict(u=[], shortcut=[], novel=[], together=[])
-        for error in errors:
-            for trajectory in error[key].keys():
-                combine_errors[key][trajectory].extend(error[key][trajectory])
-                combine_errors[key]['together'].extend(error[key][trajectory])
-
-    return combine_errors
 
 
 def get_session_errors(info, experiment_time, shuffled):
@@ -158,123 +172,6 @@ def get_session_errors(info, experiment_time, shuffled):
         return np.array([])
 
 
-def compare_rates(combined_zones, jump=0.1):
-    """Compare position normalized by time spent in zone
-
-    Parameters
-    ----------
-    combined_zones: list of OrderedDict of dict of lists
-        With experiment_time as keys and inner dict
-        has u, shortcut, novel as keys.
-    jump: float
-        Any duration above this amount will not be included.
-
-    Returns
-    -------
-    by_linger : dict
-        With u, shortcut, novel as keys.
-
-    """
-    by_linger = []
-
-    for session in combined_zones:
-        normalized = OrderedDict()
-        for experiment_time in combined_zones[0].keys():
-            normalized[experiment_time] = dict()
-
-        for experiment_time in session.keys():
-            for trajectory in session[experiment_time].keys():
-                linger = np.diff(session[experiment_time][trajectory].time)
-                linger = np.sum(linger[linger < jump])
-                normalized[experiment_time][trajectory] = session[experiment_time][trajectory].n_samples / linger
-
-        by_linger.append(normalized)
-
-    return by_linger
-
-
-def compare_lengths(infos, combined_zones, timebin=0.025):
-    """Compare position normalized by time spent in zone.
-
-    Parameters
-    ----------
-    infos: list of modules
-    combined_zones: list of OrderedDict of dict of lists
-        With experiment_time as keys and inner dict
-        has u, shortcut, novel as keys.
-
-    Returns
-    -------
-    by_track_length : list
-        Of OrderedDict with experiment_times as keys,
-        each a dict with u, shortcut, novel as keys.
-
-    """
-    lengths = dict(u=[], shortcut=[], novel=[])
-    for info in infos:
-        lengths['u'].append(LineString(info.u_trajectory).length)
-        lengths['shortcut'].append(LineString(info.shortcut_trajectory).length)
-        lengths['novel'].append(LineString(info.novel_trajectory).length)
-
-    by_track_length = []
-
-    for i, session in enumerate(combined_zones):
-        normalized = OrderedDict()
-        for exp in combined_zones[0].keys():
-            normalized[exp] = dict()
-
-        for exp in session.keys():
-            for trajectory in session[exp].keys():
-                # normalized[exp][trajectory] = (session[exp][trajectory].n_samples * timebin) / lengths[trajectory][i]
-                normalized[exp][trajectory] = (session[exp][trajectory].n_samples) / lengths[trajectory][i]
-
-        by_track_length.append(normalized)
-
-    return by_track_length
-
-
-def get_proportions(norm):
-    """Computes the proportion of normalized decoded positions
-
-    Parameters
-    ----------
-    norm : list
-        Of OrderedDict with experiment_times as keys,
-        each a dict with u, shortcut, novel as keys.
-
-    Returns
-    -------
-    proportion : list
-        Of OrderedDict with experiment_times as keys,
-        each a dict with u, shortcut, novel as keys.
-
-    """
-
-    proportion = []
-
-    for i, session in enumerate(norm):
-        session_proportion = OrderedDict()
-        n_total = OrderedDict()
-
-        for exp in norm[0].keys():
-            session_proportion[exp] = dict()
-            n_total[exp] = dict()
-
-        for exp in session.keys():
-            n_total[exp] = 0
-            for trajectory in session[exp].keys():
-                n_total[exp] += norm[i][exp][trajectory]
-
-        for exp in session.keys():
-            for trajectory in session[exp].keys():
-                n_total[exp] = np.maximum(n_total[exp], 1.0)
-                session_proportion[exp][trajectory] = norm[i][exp][trajectory] / n_total[exp]
-
-        proportion.append(session_proportion)
-
-    return proportion
-
-
 def get_combined(infos, experiment_times):
     sessions = []
 
@@ -302,50 +199,38 @@ def get_combined(infos, experiment_times):
 if __name__ == "__main__":
     from run import spike_sorted_infos, days123_infos, days456_infos, error_infos, info
 
-    infos = spike_sorted_infos
-    session = 'combined'
+    # infos = spike_sorted_infos
+    # session = 'combined'
 
-    # infos = [info.r066d3]
-    # session = 'r066d3'
+    infos = [info.r066d3]
+    session = 'r066d3'
 
     if 1:
         experiment_times = ['pauseA', 'pauseB']
-
         decodes = []
-
         for this_info in infos:
             decodes.append(load_decoded(this_info, experiment_times, pickle_filepath, get_zone_proportion))
-
         filename = os.path.join(output_filepath, session + '_decode_pauses.png')
         plot_decoded_compare(decodes, savepath=filename)
 
         experiment_times = ['phase1', 'phase2', 'phase3']
-
         decodes = []
-
         for this_info in infos:
             decodes.append(load_decoded(this_info, experiment_times, pickle_filepath, get_zone_proportion))
-
         filename = os.path.join(output_filepath, session + '_decode_phases.png')
         plot_decoded_compare(decodes, savepath=filename)
 
         experiment_times = ['prerecord', 'phase1', 'pauseA', 'phase2', 'pauseB', 'phase3', 'postrecord']
-
         decodes = []
-
         for this_info in infos:
             decodes.append(load_decoded(this_info, experiment_times, pickle_filepath, get_zone_proportion))
-
         filename = os.path.join(output_filepath, session + '_decode_all.png')
         plot_decoded_compare(decodes, savepath=filename)
 
         experiment_times = ['prerecord', 'postrecord']
-
         decodes = []
-
         for this_info in infos:
             decodes.append(load_decoded(this_info, experiment_times, pickle_filepath, get_zone_proportion))
-
         filename = os.path.join(output_filepath, session + '_decode_prepost.png')
         plot_decoded_compare(decodes, savepath=filename)
 
@@ -365,40 +250,32 @@ if __name__ == "__main__":
             filename = os.path.join(output_filepath, session + '_errors_' + experiment_time + '.png')
             plot_decoded_session_errors(errors, errors_shuffled, n_sessions, savepath=filename)
 
-    # Plot by time spent
-    if 0:
-        experiment_times = ['phase1', 'phase2', 'phase3']
-        sessions = get_combined(infos, experiment_times)
-        norm_time = compare_rates(sessions)
-        filename = os.path.join(output_filepath, 'rate_phases.png')
-        plot_decoded_compare(norm_time, ylabel='Total firing rate', savepath=filename)
-
     # Plot by track length
-    if 0:
+    if 1:
         experiment_times = ['phase1', 'phase2', 'phase3']
-        sessions = get_combined(infos, experiment_times)
-        norm_length = compare_lengths(infos, sessions)
-        proportion = get_proportions(norm_length)
+        decodes = []
+        for this_info in infos:
+            decodes.append(load_decoded(this_info, experiment_times, pickle_filepath, get_zone_proportion))
         filename = os.path.join(output_filepath, session + '_track-length_phases.png')
-        plot_decoded_compare(proportion, ylabel='Proportion relative to track length', savepath=filename)
+        plot_decoded_compare(decodes, ylabel='Proportion relative to track length', savepath=filename)
 
         experiment_times = ['pauseA', 'pauseB']
-        sessions = get_combined(infos, experiment_times)
-        norm_length = compare_lengths(infos, sessions)
-        proportion = get_proportions(norm_length)
+        decodes = []
+        for this_info in infos:
+            decodes.append(load_decoded(this_info, experiment_times, pickle_filepath, get_zone_proportion))
         filename = os.path.join(output_filepath, session + '_track-length_pauses.png')
-        plot_decoded_compare(proportion, ylabel='Proportion relative to track length', savepath=filename)
+        plot_decoded_compare(decodes, ylabel='Proportion relative to track length', savepath=filename)
 
         experiment_times = ['prerecord', 'phase1', 'pauseA', 'phase2', 'pauseB', 'phase3', 'postrecord']
-        sessions = get_combined(infos, experiment_times)
-        norm_length = compare_lengths(infos, sessions)
-        proportion = get_proportions(norm_length)
+        decodes = []
+        for this_info in infos:
+            decodes.append(load_decoded(this_info, experiment_times, pickle_filepath, get_zone_proportion))
         filename = os.path.join(output_filepath, session + '_track-length_all.png')
-        plot_decoded_compare(proportion, ylabel='Proportion relative to track length', savepath=filename)
+        plot_decoded_compare(decodes, ylabel='Proportion relative to track length', savepath=filename)
 
         experiment_times = ['prerecord', 'postrecord']
-        sessions = get_combined(infos, experiment_times)
-        norm_length = compare_lengths(infos, sessions)
-        proportion = get_proportions(norm_length)
+        decodes = []
+        for this_info in infos:
+            decodes.append(load_decoded(this_info, experiment_times, pickle_filepath, get_zone_proportion))
         filename = os.path.join(output_filepath, session + '_track-length_prepost.png')
-        plot_decoded_compare(proportion, ylabel='Proportion relative to track length', savepath=filename)
+        plot_decoded_compare(decodes, ylabel='Proportion relative to track length', savepath=filename)

utils_plotting.py
@@ -556,7 +556,7 @@ def plot_decoded_errors(decode_errors, shuffled_errors, experiment_time, fliersi
             line.set_mfc(edge_colour)
             line.set_mec(edge_colour)
 
-    ax.text(0.9, 1., 'n_sessions = ' + str(len(decode_errors)),
+    ax.text(-0.05, -0.15, 'N = ' + str(len(decode_errors)),
             verticalalignment='bottom',
             horizontalalignment='right',
             transform=ax.transAxes,
@@ -713,7 +713,7 @@ def plot_decoded_compare(decodes, ylabel='Proportion', savepath=None, transparen
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey='all', figsize=(8, 5))
 
     ind = np.arange(1)
-    width = 0.5
+    width = 0.45
     xtick_loc = []
     colours = dict(u='#0072b2', shortcut='#009e73', novel='#d55e00')
 
@@ -740,10 +740,9 @@ def plot_decoded_compare(decodes, ylabel='Proportion', savepath=None, transparen
     ax1.set_ylabel(ylabel)
     ax1.yaxis.set_ticks_position('left')
 
-    ax3.text(0.65, 0.9, 'n_sessions = ' + str(len(decodes)),
+    ax1.text(-0.05, -0.15, 'N = ' + str(len(decodes)),
              verticalalignment='bottom',
              horizontalalignment='right',
-             transform=ax3.transAxes,
              color='k', fontsize=12)
 
     plt.tight_layout()