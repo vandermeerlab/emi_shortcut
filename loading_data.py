a3aa32b571a7d398d2e77beeb3a82c1103040725
Emily Irvine <emilymirvine@gmail.com>
2018-07-09 14:46:01 -0400

    wip

--- loading_data.py
+++ loading_data.py
@@ -461,9 +461,9 @@ def plot_correcting_position(info, position, targets, events, savepath=None):
 
 if __name__ == "__main__":
     from run import spike_sorted_infos, r063_infos, r066_infos, r067_infos, r068_infos
-    import info.r068d8 as r068d8
-    infos = [r068d8]
-    # infos = spike_sorted_infos
+    # import info.r068d8 as r068d8
+    # infos = [r068d8]
+    infos = spike_sorted_infos
 
     for info in infos:
         print(info.session_id)
