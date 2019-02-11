d36080219d7abfb61ad7980c02f8fc9368d85790
emirvine <emilymirvine@gmail.com>
2019-02-11 09:18:56 -0500

    wip

--- analyze_sequenceless.py
+++ analyze_sequenceless.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.patches import Patch
 import numpy as np
+import copy
 import warnings
 import random
 import scipy
@@ -82,7 +83,7 @@ class TaskTime:
         return np.nanmax(self.likelihoods[:, :, self.zones[zone_label]], axis=2)
 
 
-def get_likelihoods(info, swr_params, task_labels, zone_labels, n_shuffles=0, save_path=None):
+def get_likelihoods(info, swr_params, task_labels, n_shuffles=0, save_path=None):
     _, position, spikes, lfp, _ = get_data(info)
 
     zones = dict()
@@ -152,7 +153,8 @@ def get_likelihoods(info, swr_params, task_labels, zone_labels, n_shuffles=0, sa
                                                    stop,
                                                    dt=t_window,
                                                    gaussian_std=0.0075,
-                                                   normalized=False)
+                                                   normalized=False,
+                                                   lastbin=True)
 
                     counts_data.append(these_counts.data)
                     counts_time.append(these_counts.time)
@@ -178,6 +180,89 @@ def get_likelihoods(info, swr_params, task_labels, zone_labels, n_shuffles=0, sa
     return session
 
 
+def limit_by_n_swr(session, task_labels, n_swr_thresh, zone_label="u"):
+    session_copy = copy.deepcopy(session)
+
+    for task_label in task_labels:
+        if getattr(session_copy, task_label).swrs.n_epochs < n_swr_thresh:
+            zone_shape = getattr(session_copy, task_label).zones[zone_label].shape
+            getattr(session_copy, task_label).likelihoods = np.ones((1, 1, zone_shape[0], zone_shape[1])) * np.nan
+
+    return session_copy
+
+
+def plot_session(sessions, title, task_labels, zone_labels, colours, filepath=None):
+
+    fig = plt.figure(figsize=(12, 6))
+    gs1 = gridspec.GridSpec(1, 4)
+    gs1.update(wspace=0.3, hspace=0.)
+
+    for i, zone_label in enumerate(zone_labels):
+        sums = {task_label: [] for task_label in task_labels}
+        n_swrs = {task_label: 0 for task_label in task_labels}
+        for session in sessions:
+            for task_label in task_labels:
+                zone_sums = getattr(session, task_label).sums(zone_label)
+                if zone_sums.size == 1:
+                    sums[task_label].extend([np.nan])
+                else:
+                    sums[task_label].extend(zone_sums)
+                    n_swrs[task_label] += getattr(session, task_label).swrs.n_epochs
+
+        for task_label in task_labels:
+            sums[task_label] = np.hstack(sums[task_label])
+
+        means = [np.nanmean(sums[task_label])
+                 if n_swrs[task_label] != 0 else 0.0
+                 for task_label in task_labels]
+
+        sems = [np.nanmean(scipy.stats.sem(sums[task_label], nan_policy="omit"))
+                if n_swrs[task_label] != 0 else 0.0
+                for task_label in task_labels]
+
+        ax = plt.subplot(gs1[i])
+        ax.bar(np.arange(sessions[0].n_tasktimes()),
+               means, yerr=sems, color=colours[zone_label])
+
+        ax.set_ylim([0, 1.])
+
+        ax.set_xticks(np.arange(sessions[0].n_tasktimes()))
+        ax.set_xticklabels(task_labels, rotation=90)
+
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+
+        if i > 0:
+            ax.set_yticklabels([])
+
+        if i == 0:
+            ax.set_ylabel("Proportion")
+
+        if zone_label == "other":
+            for n_tasktimes, task_label in enumerate(task_labels):
+                ax.text(n_tasktimes, 0.01, str(n_swrs[task_label]), ha="center", fontsize=14)
+
+    plt.text(1., 1., "n sessions: "+ str(len(sessions)), horizontalalignment='left',
+             verticalalignment='top', fontsize=14)
+
+    fig.suptitle(title, fontsize=16)
+
+    legend_elements = [Patch(facecolor=colours[zone_label], edgecolor='k', label=zone_label)
+                       for zone_label in zone_labels]
+
+    plt.legend(handles=legend_elements, bbox_to_anchor=(1., 0.95))
+
+    gs1.tight_layout(fig)
+
+    if filepath is not None:
+        plt.savefig(filepath)
+        plt.close()
+    else:
+        plt.show()
+
+
 import info.r063d2 as info
 
 # swr params
@@ -187,8 +272,17 @@ swr_params["min_length"] = 0.05
 swr_params["swr_thresh"] = (140.0, 250.0)
 swr_params["min_involved"] = 4
 
+colours = dict()
+colours["u"] = "#2b8cbe"
+colours["shortcut"] = "#31a354"
+colours["novel"] = "#d95f0e"
+colours["other"] = "#bdbdbd"
+
 task_labels = ["prerecord", "pauseA", "pauseB", "postrecord"]
 zone_labels = ["u", "shortcut", "novel", "other"]
 
-session = get_likelihoods(info, swr_params, task_labels, zone_labels, n_shuffles=0, save_path=None)
+session = get_likelihoods(info, swr_params, task_labels, n_swr_thresh=5, n_shuffles=0, save_path=None)
 print(session.pauseA.swrs.n_epochs)
+
+title = info.session_id + "_average-posterior-during-SWRs_true"
+plot_session([session], title, task_labels, zone_labels, colours)
